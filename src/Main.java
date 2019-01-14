d4e69169e742c6251d95501f3ca838c4c45948b8
416188140@qq.com <41618140@qq.com>
2019-01-14 13:57:21 +0800

    IDLE整合测试GITHUb测试

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        for (int a = 0; a <= 100; a++) {
+            if (a % 10 != 7 && a / 10 != 7) {
+                System.out.println(a);
+            }
+
+        }
         System.out.println("Hello World!");
     }
 }
